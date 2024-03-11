bcb5f9a6dd87c3433b886699b18ad6ff1a2315da
faiqxz <104989485+faiqxz@users.noreply.github.com>
2024-03-11 15:27:31 +0700

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -8,10 +8,10 @@ import {MdOutlineBusinessCenter,MdOutlinePhotoCamera,MdOutlineHealthAndSafety} f
 const Categories = () => {
   return (
     <div className='w-full bg-[#F0FBF7] py-24'>
-        <div className='md:max-w-[1480px] m-auto max-w-[600px]'>
+        <div className='md:max-w-[1480px] m-auto max-w-[600px] px-4 md:px-0'>
         <h1 className='md:leadng-[72px]  text-3xl font-bold'>Most <span className='text-[#20B486]'>Popular Categories</span></h1>
               <p className=' text-lg text-grey-600'>Various versions have evolved over the years, sometimes by accident,</p>
-              <div className=' grid lg:grid-cols-4  grid-cols-2 py-12 gap-4'>
+              <div className=' grid lg:grid-cols-4 grid-cols-2 py-12 md:gap-4 gap-1'>
                 <CategoryCard 
                     icons={<BsVectorPen size={30} />}
                     title={'Design'}
