ba4db431067c68a1c69c25ff2c5f9bd77629e4cf
faiqxz <104989485+faiqxz@users.noreply.github.com>
2024-03-11 15:27:08 +0700

    Update Achievement.jsx

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -7,7 +7,7 @@ import {LuUsers2} from 'react-icons/lu'
 const Achievement = () => {
   return (
     <div className='w-full bg-white py-24'>
-        <div className='md:max-w-[1480px] m-auto grid md:grid-cols-2 max-w-[600px]'>
+        <div className='md:max-w-[1480px] m-auto grid md:grid-cols-2 max-w-[600px]  px-4 md:px-0'>
 
           
           <div className='flex flex-col justify-center'>
