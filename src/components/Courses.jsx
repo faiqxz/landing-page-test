a5f049051d92597b705c485bf2105e1c538cd78a
faiqxz <104989485+faiqxz@users.noreply.github.com>
2024-03-11 15:28:23 +0700

    Update Courses.jsx

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -23,15 +23,19 @@ const Courses = () => {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
         }
       }
       // You can unslick at a given breakpoint now by adding:
@@ -41,9 +45,11 @@ const Courses = () => {
   };
   return (
     <div className='w-full bg-[#E9F8F3B2] py-32'>
-        <div className='md:max-w-[1480px] m-auto max-w-[600px]'>
-            <h1 className='py-3 text-3xl font-bold'>Most Popular <span className='text-[#20B486]'>Courses</span></h1>
-            <p className='text-[#6D737A]'>Various versions have evolved over the years, sometimes by accident.</p>
+        <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
+            <div className='py-4'>
+              <h1 className='py-3 text-3xl font-bold'>Most Popular <span className='text-[#20B486]'>Courses</span></h1>
+              <p className='text-[#6D737A]'>Various versions have evolved over the years, sometimes by accident.</p>
+            </div>
             <Slider {...settings}>
               {courses.map(course=><Card course={course}/>)}
 
